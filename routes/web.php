029c20f9f22da78b0b4ec59ac519bcb82c9ab695
Daniel Bara Seftino <danielbara12345@gmail.com>
2025-05-22 15:48:33 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 //import java.io;
 
 /*
@@ -24,9 +27,9 @@ Route::get('halo', function () {
 	return " <h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
 });
 
-Route::get('blog', function () {
-	return view('blog');
-});
+// Route::get('blog', function () {
+// 	return view('blog');
+// });
 
 Route::get('pertama', function () {
 	return view('pertama');
@@ -63,3 +66,14 @@ Route::get('video', function () {
 Route::get('danantara', function () {
 	return view('danantara');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('formulir', [PegawaiController::class, 'formulir']);
+Route::post('formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
